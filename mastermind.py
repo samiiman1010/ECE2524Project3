8b1f6bb9ec7c8002950af89bef249127c8cb7439
Sam Zabel <zabelsa@vt.edu>
2020-05-06 21:39:21 -0400

    Handled n response to instructions and incorrect response

--- mastermind.py
+++ mastermind.py
@@ -6,3 +6,7 @@ response = input("y/n?\n")
 
 if response == "y":
     print("Okay, so there are 8 different colors in the game: red, orange, yellow,\ngreen, blue, violet, black, and brown.  When you start the game, I will\nrandomly select a sequence of 5 colors, and some of these colors might be the same.\nYour job is to give me an initial guess on the colors I picked.\nI'll ask you for your guesses in order, because I have the colors you're guessing\norganized in a particular order that you have to match.  After you almost\ncertainly get it incorrect the first time, I'll let you know how you did; if\nyou get a correct color in a correct location, I'll let you know you'll\nguess again with a new sequence.  This process repeats until either you get it\nright or I get bored (so many 10 tries or so).  Good luck!\n\n")
+elif response == "n":
+    print("Excellent, you must be a Mastermind veteran\n\n")
+else:
+    print("Ha, gottem, I did write a handler, and you're not allowed to do that.\nSince you didn't follow the rules, I'll just assume you know what you're doing and we'll\ncontinue.\n\n")
